2079307323a6d22c7bf1535342cd0a2febbb11a4
LukasLimalkl <lukinhaslima063@gmail.com>
2023-05-26 16:13:10 -0300

    starting

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:prettier/recommended',
+      'prettier',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react'],
+  rules: {
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: 'off',
+      'lines-around-comment': [
+          'error',
+          {
+              beforeBlockComment: true,
+              afterBlockComment: true,
+              beforeLineComment: true,
+              afterLineComment: true,
+              allowBlockStart: true,
+              allowBlockEnd: true,
+              allowObjectStart: true,
+              allowObjectEnd: true,
+              allowArrayStart: true,
+              allowArrayEnd: true,
+          },
+      ],
+  },
+};
