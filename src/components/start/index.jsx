4632ac75a7b8b7722b01062628759fca34b69341
LukasLimalkl <lukinhaslima063@gmail.com>
2023-05-29 20:51:13 -0300

    add btn

--- src/components/start/index.jsx
+++ src/components/start/index.jsx
@@ -6,17 +6,31 @@ export const Start = () => {
     const periodTime = 60 * 25;
     const [timer, setTimer] = useState(periodTime);
     const [timeLeft, setTimeLeft] = useState(0);
+    const [isActive, setIsActive] = useState(false);
 
     useEffect(() => {
-        setTimeout(() => {
-            setTimer(timer - 1);
-            setTimeLeft(format(timer * 1000, 'mm:ss'));
-        }, 1000);
-    }, [timer]);
+        if (isActive) {
+            setTimeout(() => {
+                setTimer(timer - 1);
+                setTimeLeft(format(timer * 1000, 'mm:ss'));
+            }, 1000);
+        }
+    }, [timer, isActive]);
+
+    const startTimer = () => {
+        if (isActive) {
+            setIsActive(false);
+        } else {
+            setIsActive(true);
+        }
+    };
 
     return (
         <Styled.Container>
             <span>{timeLeft}</span>
+            <button onClick={startTimer}>
+                {isActive ? 'Pausar' : 'Start'}
+            </button>
         </Styled.Container>
     );
 };
