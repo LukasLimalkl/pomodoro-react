51d724274759ac22594e779cfd03c2b291dcc27d
LukasLimalkl <lukinhaslima063@gmail.com>
2023-05-29 18:31:03 -0300

    adding timer

--- src/components/start/index.jsx
+++ src/components/start/index.jsx
@@ -1,44 +1,42 @@
+import { useEffect, useState } from 'react';
 import * as Styled from './styles';
 
-// const timer = (duration) => {
-//     let min = 25;
-//     let sec = 0;
-//     duration = min * 60 + sec;
-
-//     let timer = duration;
-//     let minutes, seconds;
-//     let display;
-
-//     setInterval(() => {
-//         minutes = Math.floor(timer / 60);
-//         seconds = Math.floor(timer % 60);
-
-//         minutes = minutes < 10 ? '0' + minutes : minutes;
-//         seconds = seconds < 10 ? '0' + seconds : seconds;
-
-//         display = `${minutes} : ${seconds}`;
-//     }, 1000);
-// };
-
 export const Start = () => {
-    const timer = (duration) => {
+    const [display, setDisplay] = useState('25 : 00');
+
+    useEffect(() => {
         let min = 25;
         let sec = 0;
-        duration = min * 60 + sec;
+        let duration = min * 60 + sec;
 
         let timer = duration;
-        let minutes, seconds;
-        let display;
 
-        setInterval(() => {
-            minutes = Math.floor(timer / 60);
-            seconds = Math.floor(timer % 60);
+        const intervalId = setInterval(() => {
+            let minutes = Math.floor(timer / 60);
+            let seconds = Math.floor(timer % 60);
 
             minutes = minutes < 10 ? '0' + minutes : minutes;
             seconds = seconds < 10 ? '0' + seconds : seconds;
 
-            display = `${minutes} : ${seconds}`;
+            const newDisplay = `${minutes} : ${seconds}`;
+            setDisplay(newDisplay);
+
+            timer--;
+
+            if (timer < 0) {
+                clearInterval(intervalId);
+            }
         }, 1000);
-    };
-    return <Styled.Container>{timer}</Styled.Container>;
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <Styled.Container>
+            {display}
+            <button onClick={() => callTimer}>sta</button>
+        </Styled.Container>
+    );
 };
