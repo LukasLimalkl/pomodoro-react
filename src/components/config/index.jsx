bbbf5eafb5a5734a2f13e5ac251bd04c6931b129
LukasLimalkl <lukinhaslima063@gmail.com>
2023-06-01 20:53:38 -0300

    adding click sound

--- src/components/config/index.jsx
+++ src/components/config/index.jsx
@@ -1,17 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
+import audioClick from '../../audio/audio_click.mp3';
 import { TimerContext } from '../../context/TimerContext';
+
 import * as Styled from './styles';
 
-//eslint-disable-next-line
 export const Config = () => {
     const { resetTimer, startTimer, isActive } = useContext(TimerContext);
 
+    const audioPomo = useRef(null);
+
+    const reproduceSound = () => {
+        audioPomo.current.play();
+    };
+
     return (
         <Styled.Container>
-            <button onClick={startTimer}>
+            <button
+                onClick={() => {
+                    startTimer();
+                    reproduceSound();
+                }}
+            >
                 {isActive ? 'Pausar' : 'Start'}
             </button>
+
             <button onClick={resetTimer}>Reset</button>
+            <audio src={audioClick} ref={audioPomo} />
         </Styled.Container>
     );
 };
